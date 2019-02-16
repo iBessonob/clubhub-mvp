38069fb6ea60b6b76be5407b3502dffc554478f7
jeremyszeto <jeremyszeto@users.noreply.github.com>
2019-02-16 10:10:38 -0800

    Add skeleton for ClubActivity

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -74,19 +74,19 @@ public final class R {
         private id() {}
 
         public static final int bottom = 0x7f080021;
-        public static final int end = 0x7f080036;
-        public static final int gone = 0x7f080044;
-        public static final int invisible = 0x7f08004e;
-        public static final int left = 0x7f080050;
-        public static final int packed = 0x7f080060;
-        public static final int parent = 0x7f080061;
-        public static final int percent = 0x7f080063;
-        public static final int right = 0x7f080067;
-        public static final int spread = 0x7f080080;
-        public static final int spread_inside = 0x7f080081;
-        public static final int start = 0x7f080086;
-        public static final int top = 0x7f080096;
-        public static final int wrap = 0x7f08009c;
+        public static final int end = 0x7f08003b;
+        public static final int gone = 0x7f08004d;
+        public static final int invisible = 0x7f080057;
+        public static final int left = 0x7f080059;
+        public static final int packed = 0x7f080069;
+        public static final int parent = 0x7f08006a;
+        public static final int percent = 0x7f08006c;
+        public static final int right = 0x7f080070;
+        public static final int spread = 0x7f080089;
+        public static final int spread_inside = 0x7f08008a;
+        public static final int start = 0x7f08008f;
+        public static final int top = 0x7f08009f;
+        public static final int wrap = 0x7f0800a5;
     }
     public static final class styleable {
         private styleable() {}
